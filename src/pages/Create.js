d60c19c907f3ca5117ca39f84a6e823172e3b060
Patrik Hackl <patrik.hackl@stud.it.htl-hl.ac.at>
2023-10-05 12:35:37 +0200

    gitignore fix

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -115,7 +115,22 @@ export default function DropdownCheckbox() {
         <>
             <div id="content">
                 <div id="trainingsplananzeige">
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Dessert (100g serving)</TableCell>
+                                    <TableCell align="right">Calories</TableCell>
+                                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
 
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
 
                 </div>
                 <form onSubmit={handleSubmit} id="trainingsplanform">
