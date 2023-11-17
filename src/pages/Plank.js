e1ac2389cc0c238ad15ee39239fe35f3add29f70
Thomas Westermayer <Thomas.Westermayer@stud.it.htlhl.ac.at>
2023-11-17 13:15:12 +0100

    Design verändert

--- src/pages/Plank.js
+++ src/pages/Plank.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
-import {Stack, Zoom} from "@mui/material";
+import {Stack, Tooltip, Zoom} from "@mui/material";
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import { useTheme } from '@mui/material/styles';
@@ -15,9 +15,9 @@ import Spideranimation from '../animations/SpiderAnimation.gif'
 import Plank from '../animations/Plank.gif'
 import PlankHaende from '../animations/HaendeAnimation.gif'
 import HaendeBeinheben from '../animations/HaendeBeinheben.gif'
-import {Button} from "@material-ui/core";
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import InfoTwoToneIcon from "@mui/icons-material/InfoTwoTone";
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
@@ -61,21 +61,7 @@ function SwipeableTextMobileStepper() {
     };
 
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Paper
-                square
-                elevation={0}
-                sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    height: 50,
-                    pl: 2,
-                    bgcolor: 'background.default',
-                    justifyContent:'center',
-                }}
-            >
-                <Typography>{images[activeStep].label}</Typography>
-            </Paper>
+        <Box sx={{ flexGrow: 1 , maxWidth: 750, marginTop: 1,marginLeft: 30}}>
             <AutoPlaySwipeableViews
                 axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
                 index={activeStep}
@@ -103,6 +89,20 @@ function SwipeableTextMobileStepper() {
                     </div>
                 ))}
             </AutoPlaySwipeableViews>
+            <Paper
+                square
+                elevation={0}
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    height: 50,
+                    pl: 2,
+                    justifyContent:'center',
+                    backgroundColor: 'rgba(52, 52, 52, 0.0)'
+                }}
+            >
+                <Typography sx={{ fontSize: '20px', fontFamily: 'Arial, sans-serif' }}>{images[activeStep].label}</Typography>
+            </Paper>
             <MobileStepper
                 steps={maxSteps}
                 position="static"
@@ -113,7 +113,7 @@ function SwipeableTextMobileStepper() {
                         onClick={handleNext}
                         disabled={activeStep === maxSteps - 1}
                     >
-                        Next
+                        Nächstes Bild
                         {theme.direction === 'rtl' ? (
                             <KeyboardArrowLeft />
                         ) : (
@@ -128,7 +128,7 @@ function SwipeableTextMobileStepper() {
                         ) : (
                             <KeyboardArrowLeft />
                         )}
-                        Back
+                        Vorheriges Bild
                     </ToggleButton>
                 }
             />
@@ -145,11 +145,23 @@ export default function App() {
     };
 
     const children = [
-        <Link to={'/Plankanimation/60'}><ToggleButton value="60" key="60">60</ToggleButton></Link>,
-        <Link to={'/Plankanimation/90'}><ToggleButton value="90" key="90">90</ToggleButton></Link>,
-        <Link to={'/Plankanimation/120'}><ToggleButton value="120" key="120">120</ToggleButton></Link>,
-        <Link to={'/Plankanimation/150'}><ToggleButton value="150" key="150">150</ToggleButton></Link>,
-        <Link to={'/Plankanimation/180'}><ToggleButton value="180" key="180">180</ToggleButton></Link>,
+        <div className="timebuttons">
+            <Link to={'/Plankanimation/60'}>
+                <ToggleButton value="60" key="60" className="timebutton" style={{color:"black"}}>60</ToggleButton>
+            </Link>
+            <Link to={'/Plankanimation/90'}>
+                <ToggleButton value="90" key="90" className="timebutton" style={{color:"black"}}>90</ToggleButton>
+            </Link>
+            <Link to={'/Plankanimation/90'}>
+                <ToggleButton value="90" key="90" className="timebutton" style={{color:"black"}}>120</ToggleButton>
+            </Link>
+            <Link to={'/Plankanimation/90'}>
+                <ToggleButton value="90" key="90" className="timebutton" style={{color:"black"}}>150</ToggleButton>
+            </Link>
+            <Link to={'/Plankanimation/90'}>
+                <ToggleButton value="90" key="90" className="timebutton" style={{color:"black"}}>180</ToggleButton>
+            </Link>
+        </div>
     ];
 
     const control = {
@@ -161,13 +173,19 @@ export default function App() {
     return (
         <Zoom in={true}>
             <div id="content">
+                <div id="ueberschriftplank">Starte dein Plank-Training</div>
+                <div id="contentplank">
+                    Wähle eine Zeit und das Training startet
+                </div>
                 <div>
-                    <SwipeableTextMobileStepper />
                     <Stack spacing={3} alignItems="center">
-                        <ToggleButtonGroup {...control} aria-label="Medium sizes">
+                        <ToggleButtonGroup {...control} aria-label="Medium sizes" >
                             {children}
                         </ToggleButtonGroup>
                     </Stack>
+                    <div>
+                    <SwipeableTextMobileStepper />
+                    </div>
                 </div>
             </div>
         </Zoom>
