579477770cf01e7c0180da23f213bdb584347075
Laurin Schrammel <laurin.schrammel@stud.it.htl-hl.ac.at>
2024-03-23 08:59:10 +0100

    Fertig 2.0

--- src/pages/Cycling/Durations.js
+++ src/pages/Cycling/Durations.js
@@ -3,28 +3,28 @@ import Intensity from './Intensity';
 
 const Durations = {
     [Difficulty.EASY]: {
-        [Intensity.WARM_UP]: 1,
-        [Intensity.LOW]: 1,
-        [Intensity.MEDIUM]: 1,
-        [Intensity.HIGH]: 1
+        [Intensity.WARM_UP]: 10,
+        [Intensity.LOW]: 5,
+        [Intensity.MEDIUM]: 4,
+        [Intensity.HIGH]: 5
     },
     [Difficulty.MEDIUM]: {
-        [Intensity.WARM_UP]: 12,
-        [Intensity.LOW]: 6,
-        [Intensity.MEDIUM]: 4,
-        [Intensity.HIGH]: 2
+        [Intensity.WARM_UP]: 600,
+        [Intensity.LOW]: 25,
+        [Intensity.MEDIUM]: 5,
+        [Intensity.HIGH]: 30
     },
     [Difficulty.HARD]: {
-        [Intensity.WARM_UP]: 10,
-        [Intensity.LOW]: 30,
+        [Intensity.WARM_UP]: 600,
+        [Intensity.LOW]: 20,
         [Intensity.MEDIUM]: 5,
-        [Intensity.HIGH]: 20
+        [Intensity.HIGH]: 35
     },
     [Difficulty.ALTERNATIVE]: {
-        [Intensity.WARM_UP]: 18,
-        [Intensity.LOW]: 6,
-        [Intensity.MEDIUM]: 4,
-        [Intensity.HIGH]: 2
+        [Intensity.WARM_UP]: 600,
+        [Intensity.LOW]:15 ,
+        [Intensity.MEDIUM]: 5,
+        [Intensity.HIGH]: 40
     }
 };
 
